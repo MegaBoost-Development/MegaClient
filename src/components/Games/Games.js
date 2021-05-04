2c389f672f35614bf8e6b12395bfbd0242c4db38
Flaymed <mvigg8@gmail.com>
2021-05-03 21:56:49 -0400

    Removed bad code.

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -10,11 +10,6 @@ class Games extends Component {
 
     render() {
         return (
-
-          <main>
-            {this.props.children}
-          </main>
-
             <div className='homeWrapper'>
                 <h2>MegaClient</h2>
                 <p>Game</p>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,11 +11,6 @@ class Home extends Component {
 
     render() {
         return (
-
-          <main>
-            {this.props.children}
-          </main>
-
             <div className='homeWrapper'>
                 <h2>MegaClient</h2>
             </div>
